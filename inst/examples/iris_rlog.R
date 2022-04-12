4529219e76912922b43406db153242bb57b45caa
Peter Solymos <psolymos@gmail.com>
2022-04-12 10:35:29 -0600

    Update iris_rlog.R

--- inst/examples/iris_rlog.R
+++ inst/examples/iris_rlog.R
@@ -3,9 +3,11 @@
 #' Usage:
 #'
 #' Rscript iris_rlog.R --species virginica > iris.log 2>&1
+#' Rscript iris_rlog.R > iris.log 2>&1
 #' Rscript iris_rlog.R --species maxima > iris.log 2>&1
 #'
 #' Rscript iris_rlog.R --species virginica > iris.log 2> iris.err
+#' Rscript iris_rlog.R > iris.log 2> iris.err
 #' Rscript iris_rlog.R --species maxima > iris.log 2> iris.err
 #'
 #' Note: this script requires the rlog package
@@ -25,13 +27,18 @@ options(digits.secs = 3, error = abort)
 CONFIG <- rconfig::rconfig()
 
 info("Started")
+
 species <- rconfig::value(
     CONFIG$species,
-    error("Species not provided"))
-if (!(species %in% iris$Species))
-    error("Provide a valid species")
+    error("Species not provided")) # anticipated error
+
+## Make invalid species names a non-anticipated error
+# if (!(species %in% iris$Species))
+#     error("Provide a valid species")
+
 info("Getting summaries for species", species)
-summary(iris[iris$Species == species, 1:4])
+summary(iris[iris$Species %in% species, 1:4])
+
 info("Done")
 
 q(status = 0)
